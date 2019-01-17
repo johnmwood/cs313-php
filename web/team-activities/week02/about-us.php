cef1d9a8f4c67e13afa602fcaed13354f7f44c74
JohnMWood <jmw.home@gmail.com>
2019-01-17 10:50:33 -0700

    team-activity 02

---
+++ web/team-activities/week02/about-us.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Super Smarsh Burgers</title>
+</head>
+<body>
+    <?php
+    include 'header.php';
+    ?>
+    <h1>Super Smarsh Burgers</h1>
+    <p>When we woke up, we went to class. The little mac burger packs the biggest punch with added super armor.</p>
+</body>
+</html>
---
+++ web/team-activities/week02/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
+    <header>
+        <nav>
+        <div><img src="/images/logo.jpg" alt="logo"></div>
+        <h3>Super Smarsh Burgers</h3>
+        <ul>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="about-us.php"></a>About Us</li>
+            <li><a href="home.php">Home</a></li>
+        </ul>
+        </nav>
+    </header>
+</body>
+</html>
