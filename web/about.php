998fd0a3a78c54b5116d8ea0afbc7e7d454481ee
JohnMWood <jmw.home@gmail.com>
2019-01-19 22:39:27 -0700

    about me updated

--- web/about.php
+++ web/about.php
@@ -12,7 +12,17 @@
     <?php
     require 'header.php'; 
     ?> 
-
+    <div class="content">
+        <p>
+            My name is John Wood. I have really enjoyed working in four different 
+            internships in software engineering and data science. During the Winter
+            in Rexburg, my wife and I play board games like Scythe, Pandemic Legacy,
+            and Photosynthesis. I also really enjoy jazz. Check out 
+            <a href="https://www.youtube.com/watch?v=LxbWlDyjP9s">this new song</a> released 
+            by my favorite band, Snarky Puppy. There is a triple polyrhythm at the end 
+            and it's incredible. 
+        </p>
+    </div>
     </div>
 </body>
 </html>
