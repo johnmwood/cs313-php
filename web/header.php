ce3cc5ad20a89cb4d9b1672135477a8934f114a8
JohnMWood <jmw.home@gmail.com>
2019-01-19 23:08:31 -0700

    something

--- web/header.php
+++ web/header.php
@@ -1,28 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link rel="stylesheet" type="text/css" href="src/styles.css">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-</head>
-<body>
-    <header>
-    <?php
-    $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
-    ?>
-        <nav class="main-nav">
-        <div><img src="/images/evergreen.png" alt="logo"></div>
-        <h3>CS313 Web App</h3>
-        <ul>
-            <li class="nav-item <?php if ($file === 'about') echo 'active' ?>">
-                <a href="about.php" >About Me</a>
-            </li>
-            <li class="nav-item <?php if ($file === 'home') echo 'active' ?>">
-                <a href="home.php">Home</a>
-            </li>
-        </ul>
-        </nav> 
-    </header>
-</body>
-</html>
+<?php
+$file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
+?>
+<nav class="main-nav">
+    <div><img src="/images/evergreen.png" alt="logo"></div>
+    <h3>CS313 Web App</h3>
+    <ul>
+        <li class="nav-item <?php if ($file === 'about') echo 'active' ?>">
+            <a href="about.php" >About Me</a>
+        </li>
+        <li class="nav-item <?php if ($file === 'home') echo 'active' ?>">
+            <a href="home.php">Home</a>
+        </li>
+    </ul>
+</nav> 
