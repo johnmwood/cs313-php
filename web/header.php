ba8277b763dbd86a01aaa5bfec39690f855d8bc9
JohnMWood <jmw.home@gmail.com>
2019-01-19 23:02:52 -0700

    added php active coloring on li items

--- web/header.php
+++ web/header.php
@@ -8,12 +8,19 @@
 </head>
 <body>
     <header>
+    <?php
+    $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
+    ?>
         <nav class="main-nav">
         <div><img src="/images/evergreen.png" alt="logo"></div>
         <h3>CS313 Web App</h3>
         <ul>
-            <li><a href="about.php">About Me</a></li>
-            <li><a href="home.php">Home</a></li>
+            <li class="nav-item <?php if ($file === 'about') echo 'active' ?>">
+                <a href="about.php" >About Me</a>
+            </li>
+            <li class="nav-item <?php if ($file === 'home') echo 'active' ?>">
+                <a href="home.php">Home</a>
+            </li>
         </ul>
         </nav> 
     </header>
