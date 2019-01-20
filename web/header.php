09cb348c7f76dbdd24aaf5ba88e2d3d808cfb158
JohnMWood <jmw.home@gmail.com>
2019-01-19 22:21:31 -0700

    somethign

--- web/header.php
+++ web/header.php
@@ -10,9 +10,9 @@
     <header>
         <nav class="main-nav">
         <div><img src="/images/evergreen.png" alt="logo"></div>
-        <h3>CS313 Web Application</h3>
+        <h3>CS313 Web App</h3>
         <ul>
-            <li><a href="about.php"></a>About Us</li>
+            <li><a href="about.php">About Us</a></li>
             <li><a href="home.php">Home</a></li>
         </ul>
         </nav>
