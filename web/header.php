03851c3e59ea1ba376d2dc9e13ba181e5dc89b07
JohnMWood <jmw.home@gmail.com>
2019-01-20 11:01:10 -0700

    something

--- web/header.php
+++ web/header.php
@@ -2,14 +2,10 @@
 $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 ?>
 <nav class="main-nav">
-    <div><img src="/images/evergreen.png" alt="logo"></div>
+    <div class="logo"><a href="home.php"><img src="/images/evergreen.png" alt="logo"></a></div>
     <h3>CS313 Web App</h3>
-    <ul>
-        <li class="nav-item <?php if ($file === 'about') echo 'active' ?>">
-            <a href="about.php" >About Me</a>
-        </li>
-        <li class="nav-item <?php if ($file === 'home') echo 'active' ?>">
-            <a href="home.php">Home</a>
-        </li>
-    </ul>
+    <div>
+        <a class="nav-item <?php if ($file === 'about') echo 'active' ?>" href="about.php" >About Me</a>
+        <a class="nav-item <?php if ($file === 'home') echo 'active' ?>" href="home.php">Home</a>
+    </div>
 </nav> 
