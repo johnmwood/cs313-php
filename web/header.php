1e17b3c836e607fca7e460228d30bc3e2741bbf6
JohnMWood <jmw.home@gmail.com>
2019-01-21 07:59:19 -0700

    fixing header

--- web/header.php
+++ web/header.php
@@ -4,8 +4,6 @@ $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 <nav class="main-nav">
     <a class="logo" href="home.php"><img src="/images/evergreen.png" alt="logo">
     <h3>CS313 Web App</h3>
-    <div>
-        <a class="nav-item <?php if ($file === 'about') echo 'active' ?>" href="about.php" >About Me</a>
-        <a class="nav-item <?php if ($file === 'home') echo 'active' ?>" href="home.php">Home</a>
-    </div>
+    <a class="nav-item <?php if ($file === 'about') echo 'active' ?>" href="about.php" >About Me</a>
+    <a class="nav-item <?php if ($file === 'home') echo 'active' ?>" href="home.php">Home</a>
 </nav> 
