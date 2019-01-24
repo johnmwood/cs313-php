9a76f37c288944d4b15edff1bdb3d689329780c8
JohnMWood <jmw.home@gmail.com>
2019-01-24 13:41:09 -0700

    materialize css

--- web/header.php
+++ web/header.php
@@ -1,11 +1,24 @@
 <?php
 $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 ?>
-<nav class="main-nav">
-    <div class="logo">
-        <a href="home.php"><img src="/images/evergreen.png" alt="logo"></a> 
-        <a class="logo-title" href="home.php">CS313 Web App</a>
-    </div>
-    <a class="nav-item <?php if ($file === 'about') echo 'active' ?>" href="about.php" >About Me</a>
-    <a class="nav-item <?php if ($file === 'home') echo 'active' ?>" href="home.php">Home</a>
-</nav> 
+<nav>
+<div class="nav-wrapper">
+    <a href="#" class="brand-logo">Logo</a>
+    <ul id="nav-mobile" class="right hide-on-med-and-down">
+    <li>
+        <a class="nav-item 
+            <?php if ($file === 'about') echo 'active' ?>" 
+            href="about.php">
+            About Me
+        </a>
+    </li>
+    <li>
+        <a class="nav-item
+            <?php if ($file === 'home') echo 'active' ?>" 
+            href="home.php">
+            Home
+        </a>
+    </li>
+    </ul>
+</div>
+</nav>
