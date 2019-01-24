8a9521d7dc1f74a75fc06014a6c6d890e36f845a
JohnMWood <jmw.home@gmail.com>
2019-01-24 13:56:24 -0700

    materialize css

--- web/home.php
+++ web/home.php
@@ -8,10 +8,10 @@
     <title>Home</title>
 </head>
 <body>
-    <?php
-    require 'header.php';   
-    ?>
-    <div class="page">
+    <div className="container">
+        <?php
+            require 'header.php';   
+        ?>
         <h2>Assignments:</h2>
         <h4>under development...</h4>
     </div>
