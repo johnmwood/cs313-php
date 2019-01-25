750134cfb530a7ed84414d2a764a8809766e6a1c
JohnMWood <jmw.home@gmail.com>
2019-01-25 15:40:40 -0700

    post

--- web/header.php
+++ web/header.php
@@ -3,17 +3,17 @@
 ?>
 <nav>
 <div class="nav-wrapper teal lighten-2">
-    <a href="home.php" class="brand-logo">
+    <a href="~/home.php" class="brand-logo">
         <img src="./images/evergreen.png" alt="">CS313 PHP Web App
 </a>
     <ul id="nav-mobile" class="right hide-on-med-and-down">
     <li <?php if ($file === 'about') echo 'class="active"' ?>>
-        <a class="nav-item" href="about.php">
+        <a class="nav-item" href="~/about.php">
             About Me
         </a>
     </li>
     <li <?php if ($file === 'home') echo 'class="active"' ?>>
-        <a class="nav-item" href="home.php">
+        <a class="nav-item" href="~/home.php">
             Home
         </a>
     </li>
