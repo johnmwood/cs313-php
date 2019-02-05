c252acefa0271039525790620639d5b3ce7e56dc
JohnMWood <jmw.home@gmail.com>
2019-02-05 11:06:39 -0700

    new header for project01

--- web/project01/header.php
+++ web/project01/header.php
@@ -10,11 +10,11 @@
   <a href="home.php" class="brand-logo">
     <img src="<?php echo $root . '/images/evergreen.png'; ?>" alt="">CS313 PHP Web App
   </a>
-  <button className="btn-flat right blue">
-    Credits: <?php echo $user_credits ?>
-    <i className="material-icons">attach_money</i>
-  </button>
   <ul id="nav-mobile" class="right hide-on-med-and-down">
+    <button className="btn-flat right blue">
+      Credits: <?php echo $user_credits ?>
+      <i className="material-icons">attach_money</i>
+    </button>
     <li <?php if ($file === 'about') echo 'class="active"' ?>>
       <a class="nav-item" href="<?php echo $root . '/about.php'; ?>">
         About Me
