3c6235a054fde0295348f15930dd0403540ceebe
JohnMWood <jmw.home@gmail.com>
2019-02-12 10:57:37 -0700

    db insert team activity

--- web/team-activities/week05/insertTopics.php
+++ web/team-activities/week05/insertTopics.php
@@ -2,25 +2,31 @@
 
 require('../../db/db_connect.php');
 
-$book = $_POST["book"]; 
-$chapter = $_POST["chapter"]; 
-$verse = $_POST["verse"]; 
-$content = $_POST["content"]; 
-$topics = $_POST["topics"]; 
+try {
+  $book = $_POST["book"]; 
+  $chapter = $_POST["chapter"]; 
+  $verse = $_POST["verse"]; 
+  $content = $_POST["content"]; 
+  $topics = $_POST["topics"]; 
 
-$sql = "INSERT INTO scriptures VALUES (:book, :chapter, :verse, :content);";
-$query = $db->prepare($sql);
+  $sql = "INSERT INTO scriptures VALUES (:book, :chapter, :verse, :content);";
+  $query = $db->prepare($sql);
 
-$query->bindValue(':book', $book, PDO::PARAM_STR); 
-$query->bindValue(':chapter', $chapter, PDO::PARAM_STR); 
-$query->bindValue(':verse', $verse, PDO::PARAM_STR); 
-$query->bindValue(':content', $content, PDO::PARAM_STR); 
+  $query->bindValue(':book', $book, PDO::PARAM_STR); 
+  $query->bindValue(':chapter', $chapter, PDO::PARAM_STR); 
+  $query->bindValue(':verse', $verse, PDO::PARAM_STR); 
+  $query->bindValue(':content', $content, PDO::PARAM_STR); 
 
-$query->execute();
+  $query->execute();
 
-// $newId = $db->lastInsertId('scriptures_id_seq'); 
-// foreach($topics as $topic) {
+  // $newId = $db->lastInsertId('scriptures_id_seq'); 
+  // foreach($topics as $topic) {
 
-// }
+  // }
+} catch(PDOException $ex) { 
+  echo 'Error!: ' . $ex->getMessage();
+  echo 'RIP'; 
+  die();
+}
 
 ?> 
