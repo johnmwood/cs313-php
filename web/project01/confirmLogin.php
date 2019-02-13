3efd1de669e8e07e73eb299b981e571bc8618fb3
JohnMWood <jmw.home@gmail.com>
2019-02-13 12:10:52 -0700

    login script and page changes

--- web/project01/confirmLogin.php
+++ web/project01/confirmLogin.php
@@ -1,5 +1,30 @@
 <?php 
 require("../../db/db_connect.php");
 
+$username = $_POST["username"]; 
+$password = $_POST["password"];
+
+function checkLoginCredentials($username, $password) {
+  $sql = "SELECT username, password 
+          FROM users
+          WHERE username = :username AND password = :password";
+
+  $query = $db->prepare($sql); 
+  $query->bindValue(':username', $username, PDO::PARAM_STR);
+  $query->bindValue(':password', $password, PDO::PARAM_STR);
+
+  $query->execute();
+
+  $results = $query->fetch(); 
+
+  if ($results) {
+    $GLOBALS["username"] = $username; 
+    header("Location: main.php"); 
+  } else {
+    header("Location: login.php");
+  }
+} 
+
+checkLoginCredentials($username, $password); 
 
 ?> 
