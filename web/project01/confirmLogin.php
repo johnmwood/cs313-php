cc463208c72b1f479456819e0d7afd6afabe6753
JohnMWood <jmw.home@gmail.com>
2019-02-14 09:58:52 -0700

    login script and page changes

--- web/project01/confirmLogin.php
+++ web/project01/confirmLogin.php
@@ -1,5 +1,5 @@
 <?php 
-require("../../db/db_connect.php");
+require("../db/db_connect.php");
 
 function checkLoginCredentials($username, $password) {
   $sql = "SELECT users.username, users.password 
