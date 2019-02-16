6973fc7da59e560bd28bc71e904285f6b09ab7ea
JohnMWood <jmw.home@gmail.com>
2019-02-16 10:08:31 -0700

    fixing header

--- web/project01/header.php
+++ web/project01/header.php
@@ -8,30 +8,30 @@
   $db = connectPostgres(); 
 
   // $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
-  $loginName = $_SESSION["loginName"];
+  $userId = $_SESSION["userId"];
 
-  if ($loginName) {
+  if ($userId) {
     $query = $db->prepare("SELECT credits
                            FROM users
-                           WHERE username = :username"); 
-    $query->bindValue(':username', $loginName, PDO::PARAM_STR); 
+                           WHERE id = :id"); 
+    $query->bindValue(':id', $userId, PDO::PARAM_STR); 
     $query->execute(); 
   
     $row = $query->fetch(PDO::FETCH_ASSOC);
     $credits = $row["credits"];
   }
 
-  $homePath = ($loginName ? "./main.php" : "./login.php");
+  $homePath = ($userId ? "./main.php" : "./login.php");
 ?>
 <nav>
 <div class="nav-wrapper teal lighten-2">
-  <a href="<?php echo $homePath; ?>" class="brand-logo">
+  <a href="<?php echo $homePath; ?>" class="brand-logo left">
     <img src="<?php echo $root . '../images/evergreen.png'; ?>" alt="">Emaily
   </a>
   <ul id="nav-mobile" class="right">
     <?php 
-      if ($loginName) {
-        echo "<li><h5>" . $loginName . "</h5></li>";
+      if ($userId) {
+        echo "<li><h5>" . $userId . "</h5></li>";
         echo "<li className='btn-flat blue' style='margin: \"0 10px\"'>
                   <a class='wave-effect waves-light btn'>
                     User Credits: " . $credits . 
