a93037e787011ff99d11d64700eaabdffc9ac948
JohnMWood <jmw.home@gmail.com>
2019-02-16 17:27:42 -0700

    fixing header

--- web/db/dbConnect.php
+++ web/db/dbConnect.php
@@ -11,7 +11,7 @@ function connectPostgres() {
     $dbName = ltrim($dbOpts["path"],'/');
   
     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION, array(PDO::ATTR_PERSISTENT => true));
 
     return $db; 
   } catch (PDOException $ex) {
