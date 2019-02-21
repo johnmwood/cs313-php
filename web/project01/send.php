3c3206783a74ef88427c7ee059be50f9aa393335
JohnMWood <jmw.home@gmail.com>
2019-02-21 15:03:00 -0700

    sending emails

--- web/project01/send.php
+++ web/project01/send.php
@@ -1,5 +1,8 @@
 <?php 
 
+require('../db/dbConnect.php'); 
+
+// send email
 $title = htmlspecialchars($_POST["title"]); 
 $subject = htmlspecialchars($_POST["subject"]); 
 $body = htmlspecialchars($_POST["body"]); 
@@ -8,6 +11,16 @@ $emails = htmlspecialchars($_POST["emails"]);
 $message = "<h3>$title</h3><p>$body</p>"; 
 
 mail($emails, $subject, $message); 
+
+// subtract credits for every email sent 
+$db = connectPostgres(); 
+
+$creditsSpent = count($emails); 
+$update = "UPDATE users 
+           SET users.credits = users.credits - $creditsSpent 
+           WHERE users.id = " . $_SESSION["userId"]; 
+$statement = $db->prepare($update); 
+$statement->execute(); 
 
 header('Location: ./main.php'); 
 die(); 
