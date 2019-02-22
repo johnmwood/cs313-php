63b8d72bfe86ffced338363fb8fa65604ff1b56a
JohnMWood <jmw.home@gmail.com>
2019-02-21 22:54:10 -0700

    updated mail to not send due to heroku not supporting native php emailing from server

--- web/project01/send.php
+++ web/project01/send.php
@@ -30,13 +30,6 @@ $statement->bindValue(':credits', $creditsSpent, PDO::PARAM_INT);
 $statement->bindValue(':id', $_SESSION["userId"], PDO::PARAM_INT);  
 $statement->execute(); 
 
-// $output = "Credits spent: $creditsSpent</br>"; 
-// foreach($emails as $email) { 
-//   $output .= "Email: $email </br>";
-// }
-
-// echo "<html><body><p>" . $output . "</p></body></html>";
-
 header('Location: ./main.php'); 
 die(); 
 
